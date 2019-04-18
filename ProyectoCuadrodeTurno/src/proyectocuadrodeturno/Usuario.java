13f69f2c528e0c7cc30ed7cfdf323cd0c98554fd
Fabian Aya <Fabian Aya@DESKTOP-LOE39A9>
2019-04-17 20:54:59 -0500

    Diseño 1

--- ProyectoCuadrodeTurno/src/proyectocuadrodeturno/Usuario.java
+++ ProyectoCuadrodeTurno/src/proyectocuadrodeturno/Usuario.java
@@ -114,7 +114,7 @@ public class Usuario extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-       
+new  Registro(this, true).setVisible(true);
     }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
